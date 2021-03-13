f1273ae339fe9ed7709432670945b2993707d714
Maks <jjestertt@gmail.com>
2021-03-13 21:07:52 +0200

    Add empty component to project

---
+++ src/components/ActiveQuiz/ActiveQuiz.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./ActiveQuiz.module.scss"
+
+class ActiveQuiz extends React.Component{
+    render() {
+        return (
+            <div className={style.ActiveQuiz}>
+                <h2>ActiveQuiz</h2>
+            </div>
+        )
+    }
+}
+
+export default ActiveQuiz;
--- src/containers/Quiz/Quiz.jsx
+++ src/containers/Quiz/Quiz.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import style from "./Quiz.module.scss";
+import ActiveQuiz from "../../components/ActiveQuiz/ActiveQuiz";
 
 class Quiz extends React.Component{
+    state = {
+
+    }
     render() {
         return (
             <div className={style.Quiz}>
-                <h2>Quiz</h2>
+                <h1>Quiz</h1>
+                <ActiveQuiz />
             </div>
         )
     }
