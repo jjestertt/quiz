a2bf502583b5686473c4779300cf31e974ed4b73
Maks <jjestertt@gmail.com>
2021-03-23 19:33:27 +0200

    add form controll

--- src/containers/Auth/Auth.jsx
+++ src/containers/Auth/Auth.jsx
@@ -4,15 +4,69 @@ import style from "./Auth.module.scss";
 import Input from "../../components/UI/Input/Input";
 
 class Auth extends React.Component {
-    submitHandler(e) {
+    state = {
+        formControls: {
+            email: {
+                value: "",
+                type: "email",
+                label: "Mail",
+                errorMessage: "Введите правильно mail",
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    email: true
+                }
+            },
+            password: {
+                value: "",
+                type: "password",
+                label: "Пароль",
+                errorMessage: "Введите правильно пароль",
+                valid: false,
+                touched: false,
+                validation: {
+                    required: true,
+                    minLength6: true
+                }
+            },
+        }
+    }
+
+    submitHandler = (e) => {
         e.preventDefault();
     }
 
+    onChangeHandler = (e, controlName) =>{
+        const formControls = {...this.state.formControls}
+        formControls[controlName].value = e.target.value;
+        this.setState(formControls);
+    }
+
+    inputRenderHandler = () => {
+        return Object.keys(this.state.formControls).map((controlName, index) => {
+            const control = this.state.formControls[controlName];
+            return (
+                <Input
+                    key={controlName + index}
+                    value={control.value}
+                    onChange={(e) => {this.onChangeHandler(e, controlName)}}
+                    type={control.type}
+                    label={control.label}
+                    errorMessage={control.errorMessage}
+                    valid={control.valid}
+                    touched={control.touched}
+                    shouldValidate={!!control.validation}
+                />
+            );
+        });
+    }
+
     loginHandler = () => {
-        console.log('login');
+        console.log(this.state.formControls.email);
     }
     registerHandler = () => {
-        console.log('register');
+        console.log(this.state.formControls.password);
     }
 
     render() {
@@ -21,8 +75,10 @@ class Auth extends React.Component {
                 <div>
                     <h1>Авторизация</h1>
                     <form className={style.authForm} onSubmit={this.submitHandler}>
-                        <Input label="Почта" type="email" />
-                        <Input label="Пароль" type="password"/>
+
+                        {this.inputRenderHandler()}
+                        {/*<Input label="Почта" type="email" />*/}
+                        {/*<Input label="Пароль" type="password"/>*/}
                         <Button
                             type="success"
                             onClick={this.loginHandler}
